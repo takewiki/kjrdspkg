33221d0422a2d29ed0d0da657d0e23c3cee973e5
takewiki <hulilei@takewiki.com.cn>
2021-01-20 16:54:31 +0800

    v2.0.1

--- R/setting.R
+++ R/setting.R
@@ -6,11 +6,11 @@
 #' get_conn()
 get_conn <- function() {
   #library(tsda)
-  conn <- tsda::sql_conn_common(ip = "rm-uf6e13noj5yiye7s92o.sqlserver.rds.aliyuncs.com",
-                                port = 3433,
-                                user_name = "kangjian",
+  conn <- tsda::sql_conn_common(ip = "47.103.221.12",
+                                port = 1433,
+                                user_name = "sa",
                                 password ="kangjian@2019" ,
-                                db_name = "AIS20191122134531")
+                                db_name = "KJ2020")
   return(conn)
 
 
